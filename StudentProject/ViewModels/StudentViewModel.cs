2377f090e33ca5d3f4e46338836ac42bc55f13d3
szt3torlas <szt3torlas@SZT5-8-HP>
2024-11-20 14:52:24 +0100

    pushed;

---
+++ StudentProject/ViewModels/StudentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StudentProject.Models;
+using System.Collections.Generic;
+
+namespace StudentProject.ViewModels
+{
+    public partial class StudentViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Student _student = new Student();
+
+        [ObservableProperty]
+        ///private List<string> _educationLevels = new EducationLevels().AllEducationLevels;
+        private List<string> _educationLevels = new List<string> {"érettségi", "szakmai érettségi", "szakmai vizsga"};
+
+        public StudentViewModel()
+        {
+            _student = new Student();
+        }
+    }
+}
